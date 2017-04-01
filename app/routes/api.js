2e8385a1bd1da620ed633b5cca10dfc7b21ef563
Aditya Emani <prasaadem@gmail.com>
2017-04-01 17:40:04 -0500

    Route file Implemented

---
+++ app/routes/api.js
@@ -0,0 +1,22 @@
+var User = require('../models/user'); //User Model
+module.exports = function(router){
+
+    router.post('/users',function(req,res){
+        var user = new User();
+        user.username = req.body.username;
+        user.password = req.body.password;
+        user.email = req.body.email;
+        if(req.body.username == null || req.body.username == "" || req.body.password == null || req.body.password == "" || req.body.email == null || req.body.email == ""){
+            res.send('Enter all required information');
+        }else{
+            user.save(function(err){
+                if (err){
+                    res.send('Username or email already exits!');    
+                }else{
+                    res.send('User Created!');        
+                }
+            });
+        }
+    });
+    return router;
+}
