a2e11d03ccb33e33629a228dfbc81ae64e13f44d
Aditya Emani <prasaadem@gmail.com>
2017-04-23 15:18:05 -0500

    UI improvements in admin portal in progress

--- app/routes/api.js
+++ app/routes/api.js
@@ -34,6 +34,84 @@ var upload = multer({ dest: './app/uploads/' });
 
 module.exports = function(router) {
 
+
+
+    //http://localhost:PORT/api/addFaculty
+    //Faculty Registration Route
+
+    router.post('/addFaculty', function(req, res) {
+        var faculty = new User();
+        faculty.username = req.body.username;
+        faculty.email = req.body.email;
+        faculty.uin = req.body.uin;
+        faculty.permission = req.body.permission;
+        faculty.accountExpires = req.body.accountExpires;
+        faculty.name = req.body.name;
+        faculty.directoryName = req.body.directoryName;
+        faculty.major = req.body.major;
+        faculty.classification = req.body.classification;
+        faculty.password = req.body.password;
+        faculty.courses = [];
+
+        console.log(faculty);
+        faculty.save(function(err) {
+            if (err) {
+                res.json({
+                    success: false,
+                    message: err
+                });
+            } else {
+                res.json({
+                    success: true,
+                    message: 'Faculty Created in the database successfully!',
+                    admin: faculty
+                });
+            }
+        });
+    });
+
+    //http://localhost:PORT/api/addStudent
+    //Student Registration Route
+
+    router.post('/addStudent', function(req, res) {
+        var student = new User();
+        student.username = req.body.username;
+        student.email = req.body.email;
+        student.uin = req.body.uin;
+        student.permission = req.body.permission;
+        student.accountExpires = req.body.accountExpires;
+        student.name = req.body.name;
+        student.directoryName = req.body.directoryName;
+        student.major = req.body.major;
+        student.classification = req.body.classification;
+        student.password = req.body.password;
+        student.courses = [];
+
+        console.log(student);
+        student.save(function(err) {
+            if (err) {
+                res.json({
+                    success: false,
+                    message: err
+                });
+            } else {
+                res.json({
+                    success: true,
+                    message: 'Student Created in the database successfully!',
+                    admin: student
+                });
+            }
+        });
+    });
+
+
+
+
+
+
+
+
+
     router.post('/users', function(req, res) {
         var user = new User();
         user.username = req.body.username;
